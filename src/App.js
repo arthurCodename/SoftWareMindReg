2156682f19ebfc45b03ddb33dcc78fc470aa58a6
unknown <arthurzhuravetskiy@gmail.com>
2022-11-21 00:43:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [checked, setChecked] = useState(false);
+  const [hidden, setHidden] = useState(true);
+  const initialValues = { name: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [success, setSuccess] = useState("");
+
+  const handleCheck = (event) => {
+    if (event.target.checked) {
+      setHidden(false);
+    } else {
+      setHidden(true);
+    }
+    setChecked((current) => !current);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    if (checked) {
+      if (
+        (formValues.name !== "") &
+        (formValues.password !== "") &
+        (formValues.email !== "")
+      ) {
+        if (/@.*\./i.test(formValues.email)) {
+          setSuccess("pomyślna rejestracja");
+        } else {
+          setSuccess("błąd walidacji");
+        }
+      } else {
+        setSuccess("błąd walidacji");
+      }
+    } else {
+      if ((formValues.name !== "") & (formValues.password !== "")) {
+        setSuccess("pomyślna rejestracja");
+      } else {
+        setSuccess("błąd walidacji");
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>Name:</div>
+        <input
+          type="text"
+          name="name"
+          value={formValues.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <div>Password:</div>
+        <input
+          type="password"
+          name="password"
+          value={formValues.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          defaultChecked={checked}
+          onChange={handleCheck}
+        />
+        <span>I consent to recieving newsletters</span>
+      </div>
+      <div hidden={hidden}>
+        <div>Email:</div>
+        <input
+          type="email"
+          name="email"
+          value={formValues.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <input type="submit" onClick={handleSubmit} />
+        <div>{success}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
